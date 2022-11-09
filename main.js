161a0dc197c587b888f1e442c22309cf3408c6ef
yuldashalijalol <115680675+yuldashalijalol@users.noreply.github.com>
2022-11-09 21:29:27 +0500

    animation

---
+++ main.js
@@ -0,0 +1,29 @@
+
+alert("       Saytga hush kelibsiz")
+let name = prompt("Ismingiz:")
+let yosh = prompt("Yoshingiz:")
+let address = prompt("Manzilingiz:")
+let phone = prompt("Raqamingiz:")
+
+
+
+console.log("Ismingiz: + name")
+console.log("Yoshingiz: + yosh")
+console.log("Manzilingiz: + address")
+console.log("Raqamingiz: + phone")
+
+
+console.log("            ^   ");
+console.log("            *   ")
+console.log("           ***   ");
+console.log("          *****    ");
+console.log("         *******    ");
+console.log("        *********    ");
+console.log("       ***********    ");
+console.log("      *************    ");
+console.log("     ***************    ");
+console.log("    *****************    ");
+console.log("   *******************   ");
+console.log("  *********************   ");
+console.log("         |||||||            ");
+console.log("         |||||||             ");
